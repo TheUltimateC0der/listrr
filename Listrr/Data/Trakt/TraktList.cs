80bdf5b1d53280b17ba51798cf3ad9d3bcf94403
TheUltimateCoder <no@e.mail>
2020-02-13 22:10:28 +0100

    - Added function to get a string of all used filters - Changed description text to contain selected filters - Fixed textcolor in dropdowns (closes #72)

--- Listrr/Data/Trakt/TraktList.cs
+++ Listrr/Data/Trakt/TraktList.cs
@@ -1,7 +1,5 @@
 ﻿using Listrr.API.Trakt.Models.Filters;
 
-using Microsoft.AspNetCore.Identity;
-
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -125,6 +123,92 @@ namespace Listrr.Data.Trakt
         public StatusShowFilter ReverseFilter_Status { get; set; }
 
         #endregion
+
+
+
+        public string GetDescriptionText()
+        {
+            var result = "Normal Filters:\r\n";
+
+            if (Filter_Certifications_Movie?.Certifications != null)
+                result += $"Certifications: {string.Join(", ", Filter_Certifications_Movie?.Certifications)}\r\n";
+
+            if (Filter_Certifications_Show?.Certifications != null)
+                result += $"Certifications: {string.Join(", ", Filter_Certifications_Show?.Certifications)}\r\n";
+
+            if (Filter_Countries?.Languages != null)
+                result += $"Countries: {string.Join(", ", Filter_Countries?.Languages)}\r\n";
+
+            if (Filter_Genres?.Genres != null)
+                result += $"Genres: {string.Join(", ", Filter_Genres?.Genres)}\r\n";
+
+            if (Filter_Languages?.Languages != null)
+                result += $"Languages: {string.Join(", ", Filter_Languages?.Languages)}\r\n";
+
+            if (Filter_Networks?.Networks != null)
+                result += $"Networks: {string.Join(", ", Filter_Networks?.Networks)}\r\n";
+
+            if (Filter_Ratings != null)
+                result += $"Min rating: {Filter_Ratings?.From}\r\n";
+            if (Filter_Ratings != null)
+                result += $"Max rating: {Filter_Ratings?.To}\r\n";
+            if (Filter_Ratings != null)
+                result += $"Min votes: {Filter_Ratings?.Votes}\r\n";
+
+            if (Filter_Runtimes != null)
+                result += $"Min runtime: {Filter_Runtimes?.From}\r\n";
+            if (Filter_Runtimes != null)
+                result += $"Max runtime: {Filter_Runtimes?.To}\r\n";
+
+            if (Filter_Status?.Status != null)
+                result += $"States: {string.Join(", ", Filter_Status?.Status)}\r\n";
+
+            if (Filter_Translations?.Translations != null)
+                result += $"Translations: {string.Join(", ", Filter_Translations?.Translations)}\r\n";
+
+            if (Filter_Years != null)
+                result += $"Min year: {Filter_Years?.From}\r\n";
+            if (Filter_Years != null)
+                result += $"Max year: {Filter_Years?.To}\r\n";
+
+
+            if (ReverseFilter_Certifications_Movie?.Certifications != null ||
+                ReverseFilter_Certifications_Show?.Certifications != null ||
+                ReverseFilter_Countries?.Languages != null ||
+                ReverseFilter_Genres?.Genres != null ||
+                ReverseFilter_Languages?.Languages != null ||
+                ReverseFilter_Networks?.Networks != null ||
+                ReverseFilter_Status?.Status != null ||
+                ReverseFilter_Translations?.Translations != null)
+                result += "\r\n\r\n\r\nReverse filters:\r\n";
+
+
+            if (ReverseFilter_Certifications_Movie?.Certifications != null)
+                result += $"Certifications: {string.Join(", ", ReverseFilter_Certifications_Movie?.Certifications)}\r\n";
+
+            if (ReverseFilter_Certifications_Show?.Certifications != null)
+                result += $"Certifications: {string.Join(", ", ReverseFilter_Certifications_Show?.Certifications)}\r\n";
+
+            if (ReverseFilter_Countries?.Languages != null)
+                result += $"Countries: {string.Join(", ", ReverseFilter_Countries?.Languages)}\r\n";
+
+            if (ReverseFilter_Genres?.Genres != null)
+                result += $"Genres: {string.Join(", ", ReverseFilter_Genres?.Genres)}\r\n";
+
+            if (ReverseFilter_Languages?.Languages != null)
+                result += $"Languages: {string.Join(", ", ReverseFilter_Languages?.Languages)}\r\n";
+
+            if (ReverseFilter_Networks?.Networks != null)
+                result += $"Networks: {string.Join(", ", ReverseFilter_Networks?.Networks)}\r\n";
+
+            if (ReverseFilter_Status?.Status != null)
+                result += $"States: {string.Join(", ", ReverseFilter_Status?.Status)}\r\n";
+
+            if (ReverseFilter_Translations?.Translations != null)
+                result += $"Translations: {string.Join(", ", ReverseFilter_Translations?.Translations)}\r\n";
+            
+            return result;
+        }
 
     }
 }
