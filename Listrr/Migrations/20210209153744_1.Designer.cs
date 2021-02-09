06b800963a3fcba806a2c88c6105205993bed3b3
UltimateC0der <no@e.mail>
2021-02-09 16:42:41 +0100

    - Added new property to store report - Added migrations - Added check if movie already exists in list

--- Listrr/Migrations/20210209153744_1.Designer.cs
+++ Listrr/Migrations/20210209153744_1.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Listrr.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Listrr.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210209153744_1")]
+    partial class _1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Listrr/Migrations/20210209153744_1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Listrr.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ItemsByNameReport",
+                table: "TraktLists",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemsByNameReport",
+                table: "TraktLists");
+        }
+    }
+}
